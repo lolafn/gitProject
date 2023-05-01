519987bfd7f9ee04ec4b4a5375abf49389d86705
lola <lola.farran8@gmail.com>
2023-04-30 23:23:14 -0400

    update second line

--- scenario1.py
+++ scenario1.py
@@ -1,3 +1,3 @@
 # This is the test file for git version control system
 print("Scenario 1: Login to website.")
-print("Adding line from github")
+print("Adding line from github and updating from PyCharm")
