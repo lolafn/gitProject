985f075356a3de650448110c721ae1c2ff25fe8b
lolafn <132160786+lolafn@users.noreply.github.com>
2023-05-05 15:47:56 -0400

    Update scenario1.py

--- scenario1.py
+++ scenario1.py
@@ -1,4 +1,4 @@
 # This is the test file for git version control system
 print("Scenario 1: Login to website.")
 print("Adding line from github and updating from PyCharm")
-print("Adding more lines from GitHub. 
+print("Adding more lines from GitHub.")
