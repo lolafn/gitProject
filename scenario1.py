3ff8bf44ab3d046ee187d4ba32e54c16b15791cd
lola <lola.farran8@gmail.com>
2023-05-05 16:27:12 -0400

    updated the 3rd line and deleted the old 3rd line

--- scenario1.py
+++ scenario1.py
@@ -1,4 +1,3 @@
 # This is the test file for git version control system
 print("Scenario 1: Login to website.")
-print("Adding line from github and updating from PyCharm")
 print("Adding more lines from GitHub.")
